326e6b69ba374de4ea613fe5fabd1560830183f4
Nitish Kumar <kumarnitish4384@gmail.com>
2025-09-15 18:33:21 +0530

    changes again

--- src/app/api/auth/me/route.js
+++ src/app/api/auth/me/route.js
@@ -1,12 +1,17 @@
 // /api/auth/me/route.js
 import prisma from "@/lib/prisma";
-import { getSession } from "@/lib/session";
+import { verifyToken } from "@/lib/session";
 
-export const GET = async () => {
-    const session = getSession(); // ✅ use cookies() internally
-    console.log("session", session);
+export const GET = async (req) => {
+    console.log("req" , req)
+    const token = req.cookies.get("session")?.value;
+    console.log("token from req.cookies:", token);
 
-    if (!session) return new Response(JSON.stringify({ message: "Not authenticated" }), { status: 401 });
+    const session = token ? verifyToken(token) : null;
+
+    if (!session) {
+        return new Response(JSON.stringify({ message: "Not authenticated" }), { status: 401 });
+    }
 
     const user = await prisma.user.findUnique({
         where: { id: parseInt(session.id) },
